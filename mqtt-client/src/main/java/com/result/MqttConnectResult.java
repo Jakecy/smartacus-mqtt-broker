3a499511c715283f16d5e78d3dfb7fad86b34eba
chihaojie <woltwhite@163.com>
2020-01-01 13:52:16 +0800

    添加MqttChannelHandler

---
+++ mqtt-client/src/main/java/com/result/MqttConnectResult.java
@@ -0,0 +1,35 @@
+package com.result;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+
+/**
+ * @Author: chihaojie
+ * @Date: 2020/1/1 13:51
+ * @Version 1.0
+ * @Note
+ */
+public class MqttConnectResult {
+
+    private final boolean success;
+    private final MqttConnectReturnCode returnCode;
+    private final ChannelFuture closeFuture;
+
+    MqttConnectResult(boolean success, MqttConnectReturnCode returnCode, ChannelFuture closeFuture) {
+        this.success = success;
+        this.returnCode = returnCode;
+        this.closeFuture = closeFuture;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public MqttConnectReturnCode getReturnCode() {
+        return returnCode;
+    }
+
+    public ChannelFuture getCloseFuture() {
+        return closeFuture;
+    }
+}
