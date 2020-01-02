e70632a74fc6ee494ddaa729ffa6cd3d348b8993
chihaojie <woltwhite@163.com>
2020-01-02 18:41:26 +0800

    项目结构

---
+++ mqtt-server/src/main/java/com/auth/ClientToken.java
@@ -0,0 +1,10 @@
+package com.auth;
+
+/**
+ * @Author: chihaojie
+ * @Date: 2020/1/2 18:40
+ * @Version 1.0
+ * @Note
+ */
+public class ClientToken {
+}
---
+++ mqtt-server/src/main/java/com/persistence/TEST.java
@@ -0,0 +1,10 @@
+package com.persistence;
+
+/**
+ * @Author: chihaojie
+ * @Date: 2020/1/2 18:41
+ * @Version 1.0
+ * @Note
+ */
+public class TEST {
+}
