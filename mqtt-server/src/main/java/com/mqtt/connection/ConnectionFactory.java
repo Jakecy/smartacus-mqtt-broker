9a188b20c2ed3fed0e89ae24fcd032dc6a645dce
chihaojie <woltwhite@163.com>
2020-01-02 17:54:34 +0800

    维护全局连接工厂

--- mqtt-server/src/main/java/com/mqtt/connection/ConnectionFactory.java
+++ mqtt-server/src/main/java/com/mqtt/connection/ConnectionFactory.java
@@ -1,8 +1,11 @@
 package com.mqtt.connection;
 
 
+import com.mqtt.common.ChannelAttr;
 import com.mqtt.manager.SessionManager;
 import io.netty.channel.Channel;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Author: chihaojie
@@ -13,9 +16,23 @@ import io.netty.channel.Channel;
 public class ConnectionFactory {
 
 
- public ClientConnection create(Channel channel, SessionManager sessionManager) {
-        ClientConnection connection=new ClientConnection(channel,sessionManager);
+
+    private static  final ConcurrentHashMap<String,ClientConnection> connectionFactory=new ConcurrentHashMap<>(1024);
+
+    public    ClientConnection  create(Channel channel, SessionManager sessionManager) {
+        ClientConnection  connection=new ClientConnection(channel,sessionManager);
+        connectionFactory.put(channel.attr(ChannelAttr.ATTR_KEY_CLIENTID).get(),connection);
         return connection;
     }
+
+    public ClientConnection getConnection(String clientId){
+        ClientConnection connection = connectionFactory.get(clientId);
+        return connection;
+    }
+
+    public void removeConnection(String clientId){
+        connectionFactory.remove(clientId);
+    }
+
 
 }
