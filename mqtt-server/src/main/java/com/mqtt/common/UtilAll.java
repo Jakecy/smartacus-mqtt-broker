48d4dd201deb1249b40e342e73a99de650edc3a6
chihaojie <woltwhite@163.com>
2020-01-02 16:23:46 +0800

    增加获取线程Id的工具类

---
+++ mqtt-server/src/main/java/com/mqtt/common/UtilAll.java
@@ -0,0 +1,46 @@
+package com.mqtt.common;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+/**
+ * @Author: chihaojie
+ * @Date: 2020/1/2 16:22
+ * @Version 1.0
+ * @Note
+ */
+public class UtilAll {
+
+    public static int getPid() {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName(); // format: "pid@hostname"
+        try {
+            return Integer.parseInt(name.substring(0, name.indexOf('@')));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static void sleep(long sleepMs) {
+        if (sleepMs < 0) {
+            return;
+        }
+        try {
+            Thread.sleep(sleepMs);
+        } catch (Throwable ignored) {
+
+        }
+
+    }
+
+    public static String currentStackTrace() {
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (StackTraceElement ste : stackTrace) {
+            sb.append("\n\t");
+            sb.append(ste.toString());
+        }
+
+        return sb.toString();
+    }
+}
