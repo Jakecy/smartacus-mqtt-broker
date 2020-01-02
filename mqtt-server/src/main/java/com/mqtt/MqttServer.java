af08409866328619d9e9738798b506df4fb9983c
chihaojie <woltwhite@163.com>
2020-01-02 17:56:31 +0800

    实例化工厂

--- mqtt-server/src/main/java/com/mqtt/MqttServer.java
+++ mqtt-server/src/main/java/com/mqtt/MqttServer.java
@@ -1,6 +1,8 @@
 package com.mqtt;
 
+import com.mqtt.connection.ConnectionFactory;
 import com.mqtt.initializer.MqttChannelChannelInitializer;
+import com.mqtt.manager.SessionManager;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
@@ -23,6 +25,9 @@ public class MqttServer {
         EventLoopGroup bossGroup=new NioEventLoopGroup();
         NioEventLoopGroup  workerGroup=new NioEventLoopGroup();
         try{
+            //实例化session工厂和connection工厂
+            SessionManager sessionManager=new SessionManager();
+            ConnectionFactory connectionFactory=new ConnectionFactory();
             ServerBootstrap sboot=new ServerBootstrap();
             sboot.group(bossGroup,workerGroup)
                     //设置通道类型
